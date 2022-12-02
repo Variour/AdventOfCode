138ea7c315f4176fae808fde506b88bce1dbf7c7
variour <22418984+Variour@users.noreply.github.com>
2022-12-02 15:14:58 +0100

    initial commit

---
+++ 2022/1-1.ps1
@@ -0,0 +1,17 @@
+$inputs = Get-Content ./input.txt
+
+$highest = 0
+$current = 0
+foreach ($inp in $inputs) {
+    if ("" -eq $inp) {
+        if ($current -gt $highest) {
+            $highest = $current
+        }   
+        $current = 0
+    }
+    else {
+        $current += $inp
+    }
+}
+
+$highest
