f37346e8434e1469740491208590cabe0f0c5daf
variour <22418984+Variour@users.noreply.github.com>
2022-12-02 15:20:56 +0100

    initial commit

---
+++ 2022/1-2.ps1
@@ -0,0 +1,15 @@
+$inputs = Get-Content ./input.txt
+
+$totals = @()
+$current = 0
+foreach ($inp in $inputs) {
+    if ("" -eq $inp) {
+        $totals += $current
+        $current = 0
+    }
+    else {
+        $current += $inp
+    }
+}
+
+($totals | Sort-Object -Descending | Select-Object -First 3 | Measure-Object -Sum).Sum
